0e6c5418ac83936a5c3e469fcce0d653a3e22195
Giuliano Bortoloni <giulianoagria@hotmail.com>
2019-12-19 17:40:13 -0300

    creating method generate token in model user

--- src/models/users.js
+++ src/models/users.js
@@ -1,45 +1,50 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema(
-    {
-      nome: {
-        type: String,
-        required: true
-      },
-      email: {
-        type: String,
-        required: true
-      },
-      senha: {
-        type: String,
-        required: true
-      },
-      telefones: {
-        type: [
-          {
-            numero: {
-              type: String,
-              required: true
-            },
-            ddd: {
-              type: Number,
-              min: 2,
-              max: 3,
-              required: true
-            }
-          }
-        ],
-        required: true
-      },
-      ultimoLogin: {
-        type: Date,
-        default: Date.now
-      }
+const userSchema = new mongoose.Schema(
+  {
+    nome: {
+      type: String,
+      required: true
     },
-    { timestamps: true }
-  )
+    email: {
+      type: String,
+      required: true
+    },
+    senha: {
+      type: String,
+      required: true
+    },
+    telefones: {
+      type: [
+        {
+          numero: {
+            type: String,
+            required: true
+          },
+          ddd: {
+            type: Number,
+            min: 2,
+            max: 3,
+            required: true
+          }
+        }
+      ],
+      required: true
+    },
+    ultimoLogin: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
 );
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, "123456");
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
 
 exports.User = User;
