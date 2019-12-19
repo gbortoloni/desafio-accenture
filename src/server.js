96a0aea501fe52a16fa881b8d41c24ea77b5c22e
Giuliano Bortoloni <giulianoagria@hotmail.com>
2019-12-19 16:12:14 -0300

    creating connection in mongodb

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 
+require("./startup/db")();
+
 const port = process.env.PORT || 3003;
 app.listen(port, () => console.log(`Escutando porta ${port}...`));
